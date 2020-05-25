cfaf7b38993cc3d77894e7a340ed165dc323bdec
IsaqueAraujoApp <isaquearaujoapp@gmail.com>
2020-05-25 15:29:52 -0300

    App XLO Clone
     - Construindo o Objeto do Filtro

--- xlo/lib/screens/filter/filter_screen.dart
+++ xlo/lib/screens/filter/filter_screen.dart
@@ -13,11 +13,19 @@ class FilterScreen extends StatefulWidget {
 
 class _FilterScreenState extends State<FilterScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final ScrollController _scrollController = ScrollController();
+
+  Filter _filter = Filter(
+      maxPrice: 100,
+      minPrice: 10,
+      vendorType: VENDOR_TYPE_PARTICULAR,
+      orderBy: OrderBy.PRICE);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         elevation: 0,
         title: const Text("Filtrar busca"),
@@ -30,14 +38,21 @@ class _FilterScreenState extends State<FilterScreen> {
               padding: const EdgeInsets.all(16),
               children: <Widget>[
                 const SectionTitle(title: "Ordernar por"),
-                OrderByField(initialValue: OrderBy.DATE, onSaved: (v) {}),
+                OrderByField(
+                    initialValue: _filter.orderBy,
+                    onSaved: (v) {
+                      _filter.orderBy = v;
+                    }),
                 const SectionTitle(title: "Preço (R\$)"),
-                PriceRangeField(),
+                PriceRangeField(
+                  filter: _filter,
+                ),
                 const SectionTitle(title: "Tipo de anunciante"),
                 VendorTypeField(
-                  initialValue:
-                      VENDOR_TYPE_PARTICULAR | VENDOR_TYPE_PROFISSIONAL,
-                  onSaved: (v) {},
+                  initialValue: _filter.vendorType,
+                  onSaved: (v) {
+                    _filter.vendorType = v;
+                  },
                 ),
                 const SizedBox(
                   height: 100,
@@ -46,8 +61,21 @@ class _FilterScreenState extends State<FilterScreen> {
         ),
         AnimatedButton(
           scrollController: _scrollController,
-          onTap: (){
-            print("tocou!");
+          onTap: () {
+            if (_formKey.currentState.validate()) {
+              _formKey.currentState.save();
+              if (_filter.maxPrice != null && _filter.minPrice != null) {
+                if (_filter.minPrice > _filter.maxPrice) {
+                  _scaffoldKey.currentState.showSnackBar(
+                    SnackBar(
+                      content: const Text("Faixa de preço inválida!"),
+                      backgroundColor: Colors.pink,
+                    ),
+                  );
+                  return;
+                }
+              }
+            }
           },
         ),
       ]),
--- xlo/lib/screens/filter/widgets/price_range_field.dart
+++ xlo/lib/screens/filter/widgets/price_range_field.dart
@@ -2,8 +2,12 @@ import 'package:brasil_fields/brasil_fields.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:xlo/helpers/format_field.dart';
+import 'package:xlo/models/filter.dart';
 
 class PriceRangeField extends StatelessWidget {
+  PriceRangeField({this.filter});
+
+  final Filter filter;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -19,8 +23,13 @@ class PriceRangeField extends StatelessWidget {
               centavos: false,
             ),
           ],
-          initialValue: null,
-          onSaved: (s) {},
+          initialValue: filter.minPrice?.toString(),
+          onSaved: (s) {
+            if (s.isEmpty)
+              filter.minPrice = null;
+            else
+              filter.minPrice = int.parse(getSanitizedText(s));
+          },
           validator: (s) {
             if (s.isNotEmpty && int.tryParse(getSanitizedText(s)) == null)
               return 'Utilize valores validos';
@@ -41,8 +50,13 @@ class PriceRangeField extends StatelessWidget {
               centavos: false,
             ),
           ],
-          initialValue: null,
-          onSaved: (s) {},
+          initialValue: filter.maxPrice?.toString(),
+          onSaved: (s) {
+            if (s.isEmpty)
+              filter.maxPrice = null;
+            else
+              filter.maxPrice = int.parse(getSanitizedText(s));
+          },
           validator: (s) {
             if (s.isNotEmpty && int.tryParse(getSanitizedText(s)) == null)
               return 'Utilize valores validos';
