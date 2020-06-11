b618b00f201f9f2071cc9986687cdd545d38d680
IsaqueAraujoApp <isaquearaujoapp@gmail.com>
2020-06-11 19:31:36 -0300

    App XLO Clone
     - Finalizando o Campo de CEP

--- xlo/lib/screens/create/create_screen.dart
+++ xlo/lib/screens/create/create_screen.dart
@@ -1,7 +1,6 @@
 import 'package:brasil_fields/brasil_fields.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:xlo/api/api_postalcode.dart';
 import 'package:xlo/common/cep_field.dart';
 import 'package:xlo/common/custom_drawer/custom_drawer.dart';
 import 'package:xlo/screens/create/widgets/images_field.dart';
@@ -33,7 +32,7 @@ class _CreateScreenState extends State<CreateScreen> {
             ),
             TextFormField(
               decoration: InputDecoration(
-                labelText: 'Título =',
+                labelText: 'Título *',
                 labelStyle: TextStyle(
                   fontWeight: FontWeight.w800,
                   color: Colors.grey,
@@ -50,7 +49,7 @@ class _CreateScreenState extends State<CreateScreen> {
             TextFormField(
               maxLines: null,
               decoration: InputDecoration(
-                labelText: 'Descrição =',
+                labelText: 'Descrição *',
                 labelStyle: TextStyle(
                   fontWeight: FontWeight.w800,
                   color: Colors.grey,
@@ -67,7 +66,7 @@ class _CreateScreenState extends State<CreateScreen> {
             ),
             CepField(
               decoration: InputDecoration(
-                labelText: 'CEP*',
+                labelText: 'CEP *',
                 labelStyle: TextStyle(
                   fontWeight: FontWeight.w800,
                   color: Colors.grey,
@@ -76,12 +75,12 @@ class _CreateScreenState extends State<CreateScreen> {
                 contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
               ),
               onSaved: (c){
-
+                print(c);
               }
             ),
             TextFormField(
               decoration: InputDecoration(
-                labelText: 'Preço =',
+                labelText: 'Preço *',
                 labelStyle: TextStyle(
                   fontWeight: FontWeight.w800,
                   color: Colors.grey,
@@ -120,7 +119,6 @@ class _CreateScreenState extends State<CreateScreen> {
                   ),
                 ),
                 onPressed: () {
-                  getAdressFromAPI("20973-011");
                   if (_formkey.currentState.validate()) {
                     _formkey.currentState.save();
                   }
