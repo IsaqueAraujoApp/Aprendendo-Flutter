016de3b786a8b58681db6bc38dc646d0ecd470fb
IsaqueAraujoApp <isaquearaujoapp@gmail.com>
2020-06-15 16:24:58 -0300

    App XLO Clone
     - Adicionando os Anúncios na Tela
    Principal

--- xlo/lib/screens/create/create_screen.dart
+++ xlo/lib/screens/create/create_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:xlo/blocs/create_bloc.dart';
 import 'package:xlo/blocs/drawer_bloc.dart';
+import 'package:xlo/blocs/home_bloc.dart';
 import 'package:xlo/common/cep_field.dart';
 import 'package:xlo/common/custom_drawer/custom_drawer.dart';
 import 'package:xlo/models/ad.dart';
@@ -44,128 +45,150 @@ class _CreateScreenState extends State<CreateScreen> {
       drawer: CustomDrawer(),
       body: Form(
         key: _formkey,
-        child: ListView(
-          children: <Widget>[
-            ImagesField(
-              onSaved: (images) {
-                ad.images = images;
-              },
-              initialValue: [],
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Título *',
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  color: Colors.grey,
-                  fontSize: 18,
-                ),
-                contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
-              ),
-              validator: (text) {
-                if (text.isEmpty) return 'Campo obrigatório';
-                return null;
-              },
-              onSaved: (t) {
-                ad.title = t;
-              },
-            ),
-            TextFormField(
-              maxLines: null,
-              decoration: InputDecoration(
-                labelText: 'Descrição *',
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  color: Colors.grey,
-                  fontSize: 18,
-                ),
-                contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
-              ),
-              validator: (text) {
-                if (text.trim().isEmpty) return 'Campo obrigatório';
-                if (text.trim().length < 10) return 'Descrição muito curta';
-                return null;
-              },
-              onSaved: (d) {
-                ad.description = d;
-              },
-            ),
-            CepField(
-                decoration: InputDecoration(
-                  labelText: 'CEP *',
-                  labelStyle: TextStyle(
-                    fontWeight: FontWeight.w800,
-                    color: Colors.grey,
-                    fontSize: 18,
+        child: StreamBuilder<CreateState>(
+            stream: _createBloc.outState,
+            builder: (context, snapshot) {
+              if (snapshot.data == CreateState.LOADING)
+                return Center(
+                  child: Container(
+                    width: 300.0,
+                    height: 300.0,
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                        Colors.pink,
+                      ),
+                      strokeWidth: 5.0,
+                    ),
                   ),
-                  contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
-                ),
-                onSaved: (c) {
-                  ad.adress = c;
-                }),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Preço *',
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  color: Colors.grey,
-                  fontSize: 18,
-                ),
-                contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 16),
-              ),
-              keyboardType: const TextInputType.numberWithOptions(
-                decimal: true,
-                signed: false,
-              ),
-              inputFormatters: [
-                WhitelistingTextInputFormatter.digitsOnly,
-                RealInputFormatter(
-                  centavos: true,
-                ),
-              ],
-              validator: (text) {
-                if (text.trim().isEmpty) return 'Campo obrigatório';
-                if (int.tryParse(getSanitizedText(text)) == null)
-                  return 'Utilize valores válidos';
-                return null;
-              },
-              onSaved: (p) {
-                ad.price = int.parse(getSanitizedText(p)) / 100;
-              },
-            ),
-            HidePhoneWidget(
-              onSaved: (h) {
-                ad.hidePhone = h;
-              },
-              initialValue: false,
-            ),
-            Container(
-              height: 50,
-              child: RaisedButton(
-                color: Colors.pink,
-                child: Text(
-                  'Enviar',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
+                );
+              return ListView(
+                children: <Widget>[
+                  ImagesField(
+                    onSaved: (images) {
+                      ad.images = images;
+                    },
+                    initialValue: [],
                   ),
-                ),
-                onPressed: () async {
-                  if (_formkey.currentState.validate()) {
-                    _formkey.currentState.save();
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Título *',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        color: Colors.grey,
+                        fontSize: 18,
+                      ),
+                      contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
+                    ),
+                    validator: (text) {
+                      if (text.isEmpty) return 'Campo obrigatório';
+                      return null;
+                    },
+                    onSaved: (t) {
+                      ad.title = t;
+                    },
+                  ),
+                  TextFormField(
+                    maxLines: null,
+                    decoration: InputDecoration(
+                      labelText: 'Descrição *',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        color: Colors.grey,
+                        fontSize: 18,
+                      ),
+                      contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
+                    ),
+                    validator: (text) {
+                      if (text.trim().isEmpty) return 'Campo obrigatório';
+                      if (text.trim().length < 10)
+                        return 'Descrição muito curta';
+                      return null;
+                    },
+                    onSaved: (d) {
+                      ad.description = d;
+                    },
+                  ),
+                  CepField(
+                      decoration: InputDecoration(
+                        labelText: 'CEP *',
+                        labelStyle: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          color: Colors.grey,
+                          fontSize: 18,
+                        ),
+                        contentPadding:
+                            const EdgeInsets.fromLTRB(16, 10, 12, 10),
+                      ),
+                      onSaved: (c) {
+                        ad.adress = c;
+                      }),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Preço *',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        color: Colors.grey,
+                        fontSize: 18,
+                      ),
+                      contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 16),
+                    ),
+                    keyboardType: const TextInputType.numberWithOptions(
+                      decimal: true,
+                      signed: false,
+                    ),
+                    inputFormatters: [
+                      WhitelistingTextInputFormatter.digitsOnly,
+                      RealInputFormatter(
+                        centavos: true,
+                      ),
+                    ],
+                    validator: (text) {
+                      if (text.trim().isEmpty) return 'Campo obrigatório';
+                      if (int.tryParse(getSanitizedText(text)) == null)
+                        return 'Utilize valores válidos';
+                      return null;
+                    },
+                    onSaved: (p) {
+                      ad.price = int.parse(getSanitizedText(p)) / 100;
+                    },
+                  ),
+                  HidePhoneWidget(
+                    onSaved: (h) {
+                      ad.hidePhone = h;
+                    },
+                    initialValue: false,
+                  ),
+                  Container(
+                    height: 50,
+                    child: RaisedButton(
+                      color: Colors.pink,
+                      child: Text(
+                        'Enviar',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      onPressed: () async {
+                        if (_formkey.currentState.validate()) {
+                          _formkey.currentState.save();
 
-                    final bool success = await _createBloc.saveAd(ad);
+                          Provider.of<HomeBloc>(context).addAd(ad);
 
-                    if (success) {
-                      Provider.of<DrawerBloc>(context).setPage(0);
-                    }
-                  }
-                },
-              ),
-            ),
-          ],
-        ),
+                          final bool success = await _createBloc.saveAd(ad);
+
+                          if (success) {
+                            Provider.of<DrawerBloc>(context).setPage(0);
+                          }
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              );
+            }),
       ),
     );
   }
