b2ca05634c5b02b34a26a644be51d626dc680960
IsaqueAraujoApp <isaquearaujoapp@gmail.com>
2020-06-23 18:00:01 -0300

    App XLO Clone
     - Selecionando o Tipo do Anunciante

--- xlo/lib/models/user.dart
+++ xlo/lib/models/user.dart
@@ -1,9 +1,12 @@
-class User {
+enum UserType { PARTICULAR, PROFESSINAL }
 
-  User({this.name, this.email, this.password});
+class User {
+  User({this.name, this.email, this.password, this.phone});
 
   String name;
   String email;
   String password;
 
+  String phone;
+  UserType userType = UserType.PARTICULAR;
 }
